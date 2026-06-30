e0d9028d6611d9d350c3789850b5b21471586325
its-only-udit <udit2420u@gmail.com>
2026-06-30 22:29:27 +0530

    Add files via upload

---
+++ p4.js
@@ -0,0 +1,22 @@
+const textbox = document.getElementById("textbox");
+const tofahrenheit = document.getElementById("tofahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+
+let temp;
+
+function convert(){
+    if(tofahrenheit.checked){
+        temp = Number(textbox.value);
+        temp = temp * 9 / 5+32;
+        result.textContent = temp. toFixed(1)+"°F"
+    }
+    else if(toCelsius.checked){
+        temp = Number(textbox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp. toFixed(1)+"°C";
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+}
