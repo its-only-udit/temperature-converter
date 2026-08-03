d033798096476db62b2f9bbb2d3b25f0272f594f
its-only-udit <udit2420u@gmail.com>
2026-08-03 12:18:41 +0530

    Update p4.js

--- p4.js
+++ p4.js
@@ -5,18 +5,18 @@ const result = document.getElementById("result");
 
 let temp;
 
-function convert(){
-    if(tofahrenheit.checked){
+function convert() {
+    if (tofahrenheit.checked) {
         temp = Number(textbox.value);
-        temp = temp * 9 / 5+32;
-        result.textContent = temp. toFixed(1)+"°F"
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F"
     }
-    else if(toCelsius.checked){
+    else if (toCelsius.checked) {
         temp = Number(textbox.value);
-        temp = (temp - 32) * (5/9);
-        result.textContent = temp. toFixed(1)+"°C";
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed(1) + "°C";
     }
-    else{
+    else {
         result.textContent = "Select a unit";
     }
 }
